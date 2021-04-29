4e4f41f49509817347392504a63a77897e4445d7
mattiadebonis <mattia.debonis@gmail.com>
2021-04-29 16:43:59 +0200

    Logica css

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
+//ciclo da 1 a 100
+
+
+//stampa numero
+
+// se il resto della divisione per tre è uguale a 0 scrivi fizz
+//            se il resto della divisione per cinque scrivere fizz buzz 
+
+// se il resto della divisione per cinque è uguale a 0 scrivi buzz
