6ec6c7329116449993661d21b6819a2b09606915
mattiadebonis <mattia.debonis@gmail.com>
2021-04-29 16:52:02 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -5,10 +5,21 @@
 
 //ciclo da 1 a 100
 
+for (var i=1; i<=100; i++){
+    if (i%3 == 0){
+        if(i%5 == 0){
+            //se il resto della divisione per cinque E per tre è uguale a 0 scrivere fizz buzz 
+            console.log("Fizzbuzz")
+        }else{
+            // se il resto della divisione per tre è uguale a 0 scrivi fizz
+            console.log("Fizz")
+        }
+    }else if(i%5 == 0){
+        console.log("Buzz")
+        // se il resto della divisione per cinque è uguale a 0 scrivi buzz
+    }else{
+        console.log(i)
+        //stampa numero
+    }
+}
 
-//stampa numero
-
-// se il resto della divisione per tre è uguale a 0 scrivi fizz
-//            se il resto della divisione per cinque scrivere fizz buzz 
-
-// se il resto della divisione per cinque è uguale a 0 scrivi buzz
